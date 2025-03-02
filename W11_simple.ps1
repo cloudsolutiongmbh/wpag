6cd7cb881942e0b8d02427b3cdbe0076eb8f26ce
cloudsolutiongmbh <84773107+cloudsolutiongmbh@users.noreply.github.com>
2025-03-02 21:15:08 +0100

    Update W11_simple.ps1

--- W11_simple.ps1
+++ W11_simple.ps1
@@ -150,11 +150,11 @@ $OOBECMD = @'
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\keyboard.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\productkey.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\prereq.ps1
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F D:\autopilot.cmd
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F E:\autopilot.cmd
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F F:\autopilot.cmd
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F G:\autopilot.cmd
-start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F H:\autopilot.cmd
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F D:\autopilot.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F E:\autopilot.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F F:\autopilot.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F G:\autopilot.ps1
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F H:\autopilot.ps1
 start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\cleanlogs.ps1
 
 # Below a PS session for debug and testing in system context, # when not needed 
